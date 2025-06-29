df06cf4f0069fd6919957be33b4f17b42b357b50
amaniqbal613 <amaniqbal466@gmail.com>
2025-06-29 15:32:09 +0800

    1st program

---
+++ core java/src/june29/Program1.java
@@ -0,0 +1,12 @@
+package june29;
+
+public class Program1 {
+    public static void main (String args[]){
+        int student=54;
+        System.out.println(student);
+         student=76;
+         System.out.println("student marks "+student);
+         System.out.println(student);
+
+    }
+}
