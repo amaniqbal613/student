b119ebb6f0190a32722284d776f7d55fa944e382
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-21 16:31:49 +0800

    Relational Operator with questions

---
+++ core java/src/RelationalOperator/RelationalOperatorsSum2.java
@@ -0,0 +1,23 @@
+//int a = -4;
+//		int b = 0;
+//		boolean x = a< -5 && b==4;
+//		boolean y = --a > -6 || b-- < -1;
+//		sop(a);
+//		sop(b);
+//		sop(x);
+//		sop(y);
+//
+package RelationalOperator;
+
+public class RelationalOperatorsSum2 {
+    public static void main(String[] args) {
+        int a=-4;
+        int b=0;
+        boolean x=a<-5&&b==4;
+        boolean y=--a>-6||b--<-1;
+        System.out.println("value of a:"+a);
+        System.out.println("value of b:"+b);
+        System.out.println("value of x:"+x);
+        System.out.println("value of y:"+y);
+    }
+}
