06cab64b46f3341236f928bff257c8b933919b75
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-21 16:07:37 +0800

    Relational Operator with questions

---
+++ core java/src/RelationalOperator/RelationOperators.java
@@ -0,0 +1,21 @@
+//int a = 10;
+//		boolean x = a++==10;
+//		boolean y = --a==10;
+//		sop(a);
+//		sop(x);
+//		sop(y);
+package RelationalOperator;
+
+public class RelationOperators {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        boolean x = num1++ == 10;
+        boolean y = --num1 == 10;
+        System.out.println("value of num1:" + num1);
+        System.out.println("value of x:"+x);
+        System.out.println("value of y:"+y);
+
+    }
+
+}
---
+++ core java/src/RelationalOperator/RelationalOperators.java
@@ -0,0 +1,22 @@
+//int a = 10;
+//		int b = 3;
+//		boolean x = a++==10 && ++b==4;
+//		boolean y = --a==10 || b--<4;
+//		sop(a);
+//		sop(b);
+//		sop(x);
+//		sop(y);
+package RelationalOperator;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a=10;
+        int b=3;
+        boolean x=a++==10 && ++b==4;
+        boolean y=--a==10 ||b--<4;
+        System.out.println("value of a:"+a);
+        System.out.println("value of b:"+b);
+        System.out.println("value of x:"+x);
+        System.out.println("value of y:"+y);
+    }
+}
