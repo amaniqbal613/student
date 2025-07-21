380b48904915667f42973048c729f69395478485
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-21 16:50:57 +0800

    Relational Operator with questions

---
+++ core java/src/RelationalOperator/RelationalOperatorsSum6.java
@@ -0,0 +1,11 @@
+// int x = -5;
+//		sop("result: "+(x==-5 && ++x>-5));
+//
+package RelationalOperator;
+
+public class RelationalOperatorsSum6 {
+    public static void main(String[] args) {
+        int x=-5;
+        System.out.println("result:"+(x==-5&&++x>-5));
+    }
+}
