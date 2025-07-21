2e176c7939084102c7531b82873b9a24340f4e34
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-21 16:47:26 +0800

    Relational Operator with questions

---
+++ core java/src/RelationalOperator/RelationalOperatorsSum5.java
@@ -0,0 +1,10 @@
+//int x = 9;
+//		sop("result: "+(x==9 && ++x!=9));
+package RelationalOperator;
+
+public class RelationalOperatorsSum5 {
+    public static void main(String[] args) {
+        int x=9;
+        System.out.println("result:"+(x==9&&++x!=9));
+    }
+}
