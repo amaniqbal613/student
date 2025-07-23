36b5064a45d1108ead2d314d5c1731bdbf045dab
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-23 13:07:02 +0800

    Relational Operator with questions

---
+++ core java/src/RelationalOperator/RelationalOperatorsSum14.java
@@ -0,0 +1,16 @@
+//boolean x = false, y = true;
+//		boolean r = x==false && y!=true;
+//		sop(x);
+//		sop(y);
+//		sop(r);
+package RelationalOperator;
+
+public class RelationalOperatorsSum14 {
+    public static void main(String[] args) {
+        boolean x=false,y=true;
+        boolean r=x==false && y!=true;
+        System.out.println("value of x:"+x);
+        System.out.println("value of y:"+y);
+        System.out.println("value of r:"+r);
+    }
+}
