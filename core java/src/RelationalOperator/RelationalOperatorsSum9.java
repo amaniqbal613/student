1b9c6ac21f549c3176ffe76e5a0d60261ae383e2
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-23 13:05:54 +0800

    Relational Operator with questions

---
+++ core java/src/RelationalOperator/RelationalOperatorsSum9.java
@@ -0,0 +1,19 @@
+//int a=1, b=2, c=-3;
+//		int s = ++a + b-- + c%3 ;
+//		sop(a);
+//		sop(b);
+//		sop(c);
+//		sop(s);
+
+package RelationalOperator;
+
+public class RelationalOperatorsSum9 {
+    public static void main(String[] args) {
+        int a=1,b=2,c=-3;
+        int s=++a +b-- +c%3;
+        System.out.println("value of a:"+a);
+        System.out.println("value of b:"+b);
+        System.out.println("value of c:"+c);
+        System.out.println("value of s:"+s);
+    }
+}
