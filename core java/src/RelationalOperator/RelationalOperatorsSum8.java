d2a4598e28c73c97bcb9fb74363eb1fbe28bfc6d
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-23 13:05:36 +0800

    Relational Operator with questions

---
+++ core java/src/RelationalOperator/RelationalOperatorsSum8.java
@@ -0,0 +1,14 @@
+//int y = -1;
+//		int x = -5 * ++y ;
+//		sop(x);
+//		sop(y);
+package RelationalOperator;
+
+public class RelationalOperatorsSum8 {
+    public static void main(String[] args) {
+        int y=-1;
+        int x=-5*++y;
+        System.out.println("value of x:"+x);
+        System.out.println("value of y:"+y);
+    }
+}
