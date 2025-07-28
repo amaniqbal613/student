9f3a70889ca9172a3285e4e2c01ca2019038e6a4
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-28 19:54:32 +0800

    Logical operators with question

---
+++ core java/src/LogicalOperators/july27/FindTheValue04.java
@@ -0,0 +1,18 @@
+//int a = 1;
+//		int b = 2;
+//		if(a!=1 && b++==3){
+//			sop("hello world");
+//		}
+//		what will be the value of a and b on printing
+//
+
+package LogicalOperators.july27;
+public class FindTheValue04 {
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        if (a!=1&&b++==3){ //a condition is not correct
+            System.out.println("hello world");
+        }
+    }
+}
