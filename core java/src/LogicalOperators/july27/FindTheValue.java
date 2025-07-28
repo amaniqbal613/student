63321a081dec620b47a705fc79958723c6590ac9
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-28 19:53:45 +0800

    Logical operators with question

---
+++ core java/src/LogicalOperators/july27/FindTheValue.java
@@ -0,0 +1,25 @@
+///int a = 1;
+//		int b = 2;
+//		if(a==1 || ++b==3){
+//			sop("hello world");
+//		}
+
+package LogicalOperators.july27;
+
+public class FindTheValue {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        if (a == 1 || ++b ==3) {
+            System.out.println("hello world");
+
+        }
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+
+    }
+}
+
+
