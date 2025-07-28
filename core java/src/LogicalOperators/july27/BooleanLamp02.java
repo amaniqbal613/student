fb2fdb1264557c7e7eb29d99ebe8648bd1ef2d2d
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-28 19:55:06 +0800

    Logical operators with question

---
+++ core java/src/LogicalOperators/july27/BooleanLamp02.java
@@ -0,0 +1,21 @@
+//boolean lampX = false, result;
+//		boolean lampY = true;
+//		result = lampY || lampX;
+//		System.out.println("Lamp switch-on " + result);
+//		result = lampY | lampX;
+//		System.out.println("Lamp switch-on " + result);
+//
+package LogicalOperators.july27;
+public class BooleanLamp02 {
+    public static void main(String[] args) {
+        boolean lampX=false,result;
+        boolean lampY=true;
+        result =lampY || lampX;
+        System.out.println("Lamp switch-on "+result);
+        result=lampY|lampX;
+        System.out.println("Lamp switch-on "+result);
+    }
+}
+
+
+
