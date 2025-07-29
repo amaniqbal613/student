e4d400e63e70153b430899d3dd9a2482a2512310
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-30 02:40:21 +0800

    Relational operators with question

---
+++ core java/src/RelationalOperators/july27/Sum01.java
@@ -0,0 +1,15 @@
+//int a = 10;
+//		int b = a - 5;
+//		System.out.println(a <= b);
+//		System.out.println(a > b);
+
+package RelationalOperators.july27;
+
+public class Sum01 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=a-5;
+        System.out.println(a<=b);
+        System.out.println(a>b);
+    }
+}
