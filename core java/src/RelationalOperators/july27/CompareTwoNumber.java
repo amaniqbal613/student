38abc0f3b3ec90563ba777629b7a22a404b5cd28
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-30 02:39:56 +0800

    Relational operators with question

---
+++ core java/src/RelationalOperators/july27/CompareTwoNumber.java
@@ -0,0 +1,21 @@
+//wap to compare two numbers and print the result, >, >=, <, <=, ==, !=
+package RelationalOperators.july27;
+
+import java.util.Scanner;
+public class CompareTwoNumber {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter first number");
+        int a= scanner.nextInt();
+        System.out.println("enter second number");
+        int b= scanner.nextInt();
+        System.out.println("\nComparison Result:");
+        System.out.println(a+">"+b+":"+(a>b));
+        System.out.println(a+">="+b+":"+(a>=b));
+        System.out.println(a+"<"+b+":"+(a<b));
+        System.out.println(a+"<="+b+":"+(a<=b));
+        System.out.println(a+"=="+b+":"+(a==b));
+        System.out.println(a+"!="+b+":"+(a!=b));
+
+    }
+}
