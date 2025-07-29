e5619d4fd9fa86fa475372a1078505294b488295
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-30 02:40:31 +0800

    Relational operators with question

---
+++ core java/src/RelationalOperators/july27/Sum02.java
@@ -0,0 +1,18 @@
+//int a = 10;
+//		int b = 15;
+//		a++;
+//		b--;
+//		int c = b % a;
+//		System.out.println(c >= b);
+package RelationalOperators.july27;
+
+public class Sum02 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        a++;
+        b--;
+        int c=b%a;
+        System.out.println(c>=b);
+    }
+}
