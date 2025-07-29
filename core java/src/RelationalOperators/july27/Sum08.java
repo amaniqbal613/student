8f9fe1d0df4f1bdfed67be618fd9391589cc81b8
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-30 02:41:35 +0800

    Relational operators with question

---
+++ core java/src/RelationalOperators/july27/Sum08.java
@@ -0,0 +1,14 @@
+//int a = -1;
+//		boolean x = a!=-2 && ++a==0;
+//		sop(a);
+//		sop(x);
+package RelationalOperators.july27;
+
+public class Sum08 {
+    public static void main(String[] args) {
+        int a=-1;
+        boolean x=a!=-2 && ++a==0;
+        System.out.println("value of a "+a);
+        System.out.println("Condition in x "+x);
+    }
+}
