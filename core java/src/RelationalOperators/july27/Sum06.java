a7158371ab40dde9017395d4d4fc7cd794cd2530
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-30 02:41:15 +0800

    Relational operators with question

---
+++ core java/src/RelationalOperators/july27/Sum06.java
@@ -0,0 +1,22 @@
+//int a = -4;
+//		int b = 0;
+//		boolean x = a< -5 && b==4;
+//		boolean y = --a > -6 || b-- < -1;
+//		sop(a);
+//		sop(b);
+//		sop(x);
+//		sop(y);
+package RelationalOperators.july27;
+
+public class Sum06 {
+    public static void main(String[] args) {
+        int a=-4;
+        int b=0;
+        boolean x=a<-5&&b==4;
+        boolean y=--a>-6||b--<-1;
+        System.out.println("value of a "+a);
+        System.out.println("value of b "+b);
+        System.out.println("Condition in x "+x);
+        System.out.println("Condition in y "+y);
+    }
+}
