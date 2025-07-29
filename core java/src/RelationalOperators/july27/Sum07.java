678826bd532679220d047d950a47480d3c94449d
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-30 02:41:25 +0800

    Relational operators with question

---
+++ core java/src/RelationalOperators/july27/Sum07.java
@@ -0,0 +1,22 @@
+//int a = -1;
+//		int b = 1;
+//		boolean x = ++a<=0 && ++b>1;
+//		boolean y = --a > -2 || b-- < 1;
+//		sop(a);
+//		sop(b);
+//		sop(x);
+//		sop(y);
+package RelationalOperators.july27;
+
+public class Sum07 {
+    public static void main(String[] args) {
+        int a = -1;
+        int b = 1;
+        boolean x = ++a<=0 && ++b>1;
+        boolean y = --a > -2 || b-- < 1;
+        System.out.println("value of a "+a);
+        System.out.println("value of b "+b);
+        System.out.println("Condition in x "+x);
+        System.out.println("Condition in y "+y);
+    }
+}
