caadac243ee17d7ce192d4f130ca2a0dc22928b1
amaniqbal613 <amaniqbal466@gmail.com>
2025-07-30 02:40:44 +0800

    Relational operators with question

---
+++ core java/src/RelationalOperators/july27/Sum03.java
@@ -0,0 +1,18 @@
+//int a = 10;
+//		boolean x = a++==10;
+//		boolean y = --a==10;
+//		sop(a);
+//		sop(x);
+//		sop(y);
+package RelationalOperators.july27;
+
+public class Sum03 {
+    public static void main(String[] args) {
+        int a=10;
+        boolean x=a++==10;
+        boolean y=--a==10;
+        System.out.println("value of "+a);
+        System.out.println("Condition in x "+x);
+        System.out.println("Condition in y "+y);
+    }
+}
