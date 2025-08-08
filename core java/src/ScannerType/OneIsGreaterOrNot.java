e9c2f659b691b6c081ab3da6c2bc7820b62558ed
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-08 23:46:33 +0530

    If or else with question

---
+++ core java/src/ScannerType/OneIsGreaterOrNot.java
@@ -0,0 +1,16 @@
+//if( 1 > 2 ) {
+//		    System.out.println(" 1 is greater than 2");
+//		}
+package ScannerType;
+
+public class OneIsGreaterOrNot {
+    public static void main(String[] args) {
+        int num1=1;
+         int num2=2;
+         if (1>2){
+             System.out.println("1 is greater than 2");
+         }
+         else
+             System.out.println("1 is not greater than 2");
+    }
+}
