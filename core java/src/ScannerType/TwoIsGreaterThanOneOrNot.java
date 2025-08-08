906d6393c1033498e826f9fb9264aa59f32d051c
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-08 23:49:30 +0530

    If or else with question

---
+++ core java/src/ScannerType/TwoIsGreaterThanOneOrNot.java
@@ -0,0 +1,20 @@
+//if( 1 < 2 ) {
+//		    System.out.println("1 is less than 2");
+//		}
+//		else
+//		    System.out.println("2 is less than 1");
+//		    System.out.println("Hello From IfExample");
+package ScannerType;
+
+
+public class TwoIsGreaterThanOneOrNot {
+    public static void main(String[] args) {
+        if( 1 < 2 ) {
+            System.out.println("1 is less than 2");
+        }
+        else
+            System.out.println("2 is less than 1");
+        System.out.println("Hello From IfExample");
+    }
+
+}
