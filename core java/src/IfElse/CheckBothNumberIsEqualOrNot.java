6623cd26edaf7fcb0a2b750ae810c8144bf6f0ff
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-09 23:49:21 +0530

    If or else with question

---
+++ core java/src/IfElse/CheckBothNumberIsEqualOrNot.java
@@ -0,0 +1,19 @@
+//21.	float fl = 5.3f;
+//		if (fl == 5.3)
+//		    System.out.println("Both are equal");
+//		else
+//		    System.out.println("Both are not equal");
+package IfElse;
+
+public class CheckBothNumberIsEqualOrNot {
+    public static void main(String[] args) {
+
+
+        float fl = 5.3f;
+        if (fl == 5.3) {
+            System.out.println("Both are equal");
+        }
+        else
+            System.out.println("Both are not equal");
+    }
+}
