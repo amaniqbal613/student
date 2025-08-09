afdbe7c10d7e7dddcde6adc3aab25ca32cbc1102
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-10 00:02:37 +0530

    If or else with question

---
+++ core java/src/IfElse/CheckTrueorFalse.java
@@ -0,0 +1,20 @@
+//boolean x = true;
+//		boolean y = false;
+//		if (x && y) {
+//		    System.out.println(true);
+//		} else {
+//		    System.out.println(false);
+//		}
+package IfElse;
+
+public class CheckTrueorFalse {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+        if (x && y) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
