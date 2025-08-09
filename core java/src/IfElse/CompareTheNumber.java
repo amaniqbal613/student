c58a2cb544e950f0a6ceea443c54123c677dfef2
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-10 00:02:24 +0530

    If or else with question

---
+++ core java/src/IfElse/CompareTheNumber.java
@@ -0,0 +1,16 @@
+//int x = 20;
+//		int y = 25;
+//		if (++x < (y = y -= 4) || (x = x += 4) > y) {
+//		    System.out.println(x + "," + y);
+//		}
+package IfElse;
+
+public class CompareTheNumber {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 25;
+        if (++x < (y = y -= 4) || (x = x += 4) > y) {
+            System.out.println(x + "," + y);
+        }
+    }
+    }
