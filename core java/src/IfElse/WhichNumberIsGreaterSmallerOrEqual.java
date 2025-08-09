88483baab50b23e2a893adbd0981e9a69ef23ec3
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-09 23:43:37 +0530

    If or else with question

---
+++ core java/src/IfElse/WhichNumberIsGreaterSmallerOrEqual.java
@@ -0,0 +1,31 @@
+//int a = 7 * 3 + 6 / 2 - 5;
+//		int b = 21 - 8 + a % 3 * 11;
+//		if(a < b) {
+//		    System.out.println("A is less than B");
+//		}
+//		if(a = b) {
+//		    System.out.println("A is equal to B");
+//		}
+//		if(a > b) {
+//		    System.out.println("A is greater than B");
+//		}
+package IfElse;
+
+
+public class WhichNumberIsGreaterSmallerOrEqual {
+    public static void main(String[] args) {
+        int a = 7 * 3 + 6 / 2 - 5;
+        int b = 21 - 8 + a % 3 * 11;
+        if(a < b) {
+            System.out.println("A is less than B");
+        }
+        if(a==b) {
+            System.out.println("A is equal to B");
+        }
+        if(a > b) {
+            System.out.println("A is greater than B");
+        }
+
+    }
+
+}
