a628537ae692b0d5777691f8af30d84cda0df8c4
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-14 13:59:12 +0530

    If or else with question

---
+++ core java/src/IfElse/FalseOrTrue.java
@@ -0,0 +1,30 @@
+//boolean a=true, b = false;
+//		if (a) {
+//			System.out.println("A");
+//		} else if (a && b) {
+//			System.out.println("A && B");
+//		} else {
+//			if (!b) {
+//				System.out.println("notB");
+//			} else {
+//				System.out.println("ELSE");
+//			}
+//		}
+package IfElse;
+
+public class FalseOrTrue {
+    public static void main(String[] args) {
+        boolean a=true, b = false;
+        if (a) {
+            System.out.println("A");
+        } else if (a && b) {
+            System.out.println("A && B");
+        } else {
+            if (!b) {
+                System.out.println("notB");
+            } else {
+                System.out.println("ELSE");
+            }
+        }
+    }
+}
