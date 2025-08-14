cb0362f009ec0ae87fd8ec98a716f4c42974e736
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-14 13:55:16 +0530

    If or else with question

---
+++ core java/src/IfElse/PassOrFail02.java
@@ -0,0 +1,20 @@
+//int marks = 50;
+//		if( marks >= 70 )
+//		    System.out.println("Distinction");
+//		if( marks < 70 && marks >= 35 )
+//		    System.out.println("Pass");
+//		if( marks < 35 )
+//		    System.out.println("Fail");
+package IfElse;
+
+public class PassOrFail02 {
+    public static void main(String[] args) {
+        int marks = 50;
+        if( marks >= 70 )
+            System.out.println("Distinction");
+        if( marks < 70 && marks >= 35 )
+            System.out.println("Pass");
+        if( marks < 35 )
+            System.out.println("Fail");
+    }
+}
