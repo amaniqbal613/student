2a975ff45895e9e28eef6e76ca42d535076d39e4
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-19 01:12:14 +0530

    If or else with question

---
+++ core java/src/IfElse/WhichNumberIsGreater.java
@@ -0,0 +1,19 @@
+//	Wap to find greatest number among two numbers
+package IfElse;
+
+import java.util.Scanner;
+
+public class WhichNumberIsGreater {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the first number");
+        int num1= scanner.nextInt();
+        System.out.println("enter the second number");
+        int num2= scanner.nextInt();
+        if(num1>num2){
+            System.out.println(num1+" is greater ");
+        }
+        else
+            System.out.println(num2+" is greater");
+    }
+}
