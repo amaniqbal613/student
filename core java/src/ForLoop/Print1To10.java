ffcf02a82a41df2a36d48b5fe710045889286e27
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-25 19:44:13 +0530

    If or else with question

---
+++ core java/src/ForLoop/Print1To10.java
@@ -0,0 +1,11 @@
+//wap to print number from 1 to 10
+package ForLoop;
+
+public class Print1To10 {
+    public static void main(String[] args) {
+        System.out.println("print number 1 to 10");
+        for (int i=1;i<=10;i++){
+            System.out.println("i-"+i);
+        }
+    }
+}
