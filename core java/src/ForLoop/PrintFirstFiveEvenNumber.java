4582addd384370e42530cd70b3fac5b47520ab40
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-25 20:05:36 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintFirstFiveEvenNumber.java
@@ -0,0 +1,11 @@
+//wap to print first five even numbers.
+package ForLoop;
+
+public class PrintFirstFiveEvenNumber {
+    public static void main(String[] args) {
+        System.out.println("first five even number");
+        for (int i=2;i<=10;i+=2){
+            System.out.println("i-"+i);
+        }
+    }
+}
