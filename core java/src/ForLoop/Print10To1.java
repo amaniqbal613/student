db9c6bc4ae4eff53d5af7254f0b3493362f33f91
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-25 19:44:55 +0530

    For Loop with question

---
+++ core java/src/ForLoop/Print10To1.java
@@ -0,0 +1,11 @@
+//2.	wap to print number from 10 to 1
+package ForLoop;
+
+public class Print10To1 {
+    public static void main(String[] args) {
+        System.out.println("printing number from 10 to 1");
+        for(int i=10;i>=1;i--){
+            System.out.println("i-"+i);
+        }
+    }
+}
