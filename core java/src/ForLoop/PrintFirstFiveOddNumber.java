641c2d506bd52939a09ae3ef98111c5fe9a2ebe0
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-25 20:00:50 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintFirstFiveOddNumber.java
@@ -0,0 +1,11 @@
+//wap to print first five odd numbers.
+package ForLoop;
+
+public class PrintFirstFiveOddNumber {
+    public static void main(String[] args) {
+        System.out.println("first five odd number");
+        for (int i=1;i<=9;i+=2){
+            System.out.println("1-"+i);
+        }
+    }
+}
