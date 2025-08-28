52114039c7ba65ed3daaaf508abf3732f5e1f51c
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-28 12:53:27 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintNegativeNumber.java
@@ -0,0 +1,11 @@
+//wap to print to print all negative numbers between -1 and -10.
+package ForLoop;
+
+public class PrintNegativeNumber {
+public static void main(String[] args) {
+    System.out.println("print negative number");
+    for (int i=-1;i>=-10;i--){
+        System.out.println("i="+i);
+    }
+}
+}
