9c452a5905d507c1fff7f1fb844d8961ac0b2c3f
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-30 11:32:18 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintTenTimesWorld.java
@@ -0,0 +1,11 @@
+//wap to print 10 times wolrd.
+package ForLoop;
+
+public class PrintTenTimesWorld {
+    public static void main(String[] args) {
+        System.out.println("print world");
+        for (int i=1;i<=10;i++){
+            System.out.println("world");
+        }
+    }
+}
