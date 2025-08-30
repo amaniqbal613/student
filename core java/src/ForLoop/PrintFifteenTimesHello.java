b0e607ac5b2441443aeb0912f6ec2a79b3c38a08
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-30 11:31:31 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintFifteenTimesHello.java
@@ -0,0 +1,11 @@
+//wap to print 15 times hello.
+package ForLoop;
+
+public class PrintFifteenTimesHello {
+    public static void main(String[] args) {
+        System.out.println("print hello");
+        for (int i=1;i<=15;i++){
+            System.out.println("HELLO");
+        }
+    }
+}
