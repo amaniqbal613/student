55dadb8f6a4ada578bf56dae29695991ae194bcc
amaniqbal613 <amaniqbal466@gmail.com>
2025-08-30 11:31:57 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintFiveTimesA.java
@@ -0,0 +1,12 @@
+//wap to print 5 times A.
+package ForLoop;
+
+
+public class PrintFiveTimesA {
+public static void main(String[] args) {
+    System.out.println("print A");
+    for (int i=1;i<=5;i++){
+        System.out.println("A");
+    }
+}
+}
