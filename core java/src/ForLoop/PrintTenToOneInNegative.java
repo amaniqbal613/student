51cd4384ab0aed5312d7ed637ef5657a751dde36
amaniqbal613 <amaniqbal466@gmail.com>
2025-09-05 19:37:03 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintTenToOneInNegative.java
@@ -0,0 +1,11 @@
+//wap to print -10 to -1.
+package ForLoop;
+
+public class PrintTenToOneInNegative {
+    public static void main(String[] args) {
+        System.out.println("print -10 to -1");
+        for (int i=-10;i<=-1;i++){
+            System.out.println(i);
+        }
+    }
+}
