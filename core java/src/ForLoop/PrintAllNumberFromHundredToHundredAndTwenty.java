d51a72a98e8e33de982d521fcfae5d3330a09d34
amaniqbal613 <amaniqbal466@gmail.com>
2025-09-05 19:42:29 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintAllNumberFromHundredToHundredAndTwenty.java
@@ -0,0 +1,11 @@
+//wap to print all even numbers between 100 to 120.
+package ForLoop;
+
+public class PrintAllNumberFromHundredToHundredAndTwenty {
+    public static void main(String[] args) {
+        System.out.println("number between 100 to 120");
+        for (int i=100;i<=120;i++){
+            System.out.println(i);
+        }
+    }
+}
