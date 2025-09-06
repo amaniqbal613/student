a0076217a7ba056349e9cfbb68ce5272437cd0df
amaniqbal613 <amaniqbal466@gmail.com>
2025-09-06 17:15:07 +0530

    For Loop with question

---
+++ core java/src/ForLoop/NumberBetweenHundredToOneThoseAreNotDivisibleByElevenAndThree.java
@@ -0,0 +1,13 @@
+//wap to print all numbers between 100 to 1 those are not divisible by 11 and 3.
+package ForLoop;
+
+public class NumberBetweenHundredToOneThoseAreNotDivisibleByElevenAndThree {
+    public static void main(String[] args) {
+        System.out.println("all numbers between 100 to 1 those are not divisible by 11 and 3.");
+        for (int i=100;i>=1;i--){
+            if (i%11!=0 && i%3!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
