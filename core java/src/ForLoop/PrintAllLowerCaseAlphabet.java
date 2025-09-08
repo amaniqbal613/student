bfe3f9c2d0ddb15754e178c18cb5569eb561b0b4
amaniqbal613 <amaniqbal466@gmail.com>
2025-09-08 23:44:52 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintAllLowerCaseAlphabet.java
@@ -0,0 +1,11 @@
+//wap to print all lower case alphabets.
+package ForLoop;
+
+public class PrintAllLowerCaseAlphabet {
+    public static void main(String[] args) {
+        System.out.println("print all lower case alphabet");
+        for (char c='a';c<='z';c++){
+            System.out.println(c);
+        }
+    }
+}
