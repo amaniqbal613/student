7a709b0c97cf654f4be03f8dfb603aa36b33cec6
amaniqbal613 <amaniqbal466@gmail.com>
2025-09-08 23:49:34 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintAllUpperCaseAlphabet.java
@@ -0,0 +1,11 @@
+//wap to print all upper case alphabets.
+package ForLoop;
+
+public class PrintAllUpperCaseAlphabet {
+    public static void main(String[] args) {
+        System.out.println(" print all upper case alphabets.");
+        for (char c='A';c<='Z';c++){
+            System.out.println(c);
+        }
+    }
+}
