a188864ec03f406dcb467b09e86d7b5c419b4917
amaniqbal613 <amaniqbal466@gmail.com>
2025-09-23 23:10:54 +0530

    For Loop with question

---
+++ core java/src/ForLoop/FactorialOfANumber.java
@@ -0,0 +1,22 @@
+//wap to find factorial of a given number
+package ForLoop;
+
+import java.util.Scanner;
+
+public class FactorialOfANumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        long factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+
+        System.out.println("Factorial of " + num + " is: " + factorial);
+    }
+}
