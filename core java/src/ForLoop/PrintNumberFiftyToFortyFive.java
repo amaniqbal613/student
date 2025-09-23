a28a3204e838dd080f59fb0e1ae4312d1eaf4304
amaniqbal613 <amaniqbal466@gmail.com>
2025-09-23 23:02:10 +0530

    For Loop with question

---
+++ core java/src/ForLoop/PrintNumberFiftyToFortyFive.java
@@ -0,0 +1,10 @@
+//wap to print number from 50 to 45
+package ForLoop;
+
+public class PrintNumberFiftyToFortyFive {
+    public static void main(String[] args) {
+        for (int i=50;i>=45;i--){
+            System.out.println(i);
+        }
+    }
+}
