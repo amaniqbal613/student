bd2404dc97d7a60759825a2fe1813160e8bcf473
amaniqbal613 <amaniqbal466@gmail.com>
2025-09-23 23:18:03 +0530

    For Loop with question

---
+++ core java/src/ForLoop/FibonocciSeries.java
@@ -0,0 +1,24 @@
+//wap to print fibonocci series
+package ForLoop;
+
+import java.util.Scanner;
+
+public class FibonocciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
