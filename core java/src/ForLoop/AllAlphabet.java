0322a8757f4e2b8f6c7ccc5bbb291f48d2d3580b
amaniqbal613 <amaniqbal466@gmail.com>
2025-10-01 12:23:26 +0530

    For Loop with question

---
+++ core java/src/ForLoop/AllAlphabet.java
@@ -0,0 +1,10 @@
+//48.	Wap to print all alphabets from a to z.
+package ForLoop;
+
+public class AllAlphabet {
+    public static void main(String[] args) {
+        for (char c='a';c<='z';c++){
+            System.out.println(c);
+        }
+    }
+}
