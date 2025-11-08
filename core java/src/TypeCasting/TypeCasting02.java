babd81b88990463016170ae1066a83e72d989bce
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-08 23:14:31 +0530

    TypeCasting with question

---
+++ core java/src/TypeCasting/TypeCasting02.java
@@ -0,0 +1,10 @@
+//	2.	wap to create an int variable but assign a char value and print that variable?
+package TypeCasting;
+
+public class TypeCasting02 {
+    public static void main(String[] args) {
+        int num=93;
+        char c=(char)num;
+        System.out.println("result is="+c);
+    }
+}
