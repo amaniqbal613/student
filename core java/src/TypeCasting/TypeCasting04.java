d07da1999489869cfbd2f00ab94776af443aaae5
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-08 23:23:19 +0530

    TypeCasting with question

---
+++ core java/src/TypeCasting/TypeCasting04.java
@@ -0,0 +1,10 @@
+//	5.	wap to define a short variable and assign its value to a byte type variable and print both variables.
+package TypeCasting;
+
+public class TypeCasting04 {
+    public static void main(String[] args) {
+        short s=230;
+        byte b=(byte)s;
+        System.out.println("result="+b);
+    }
+}
