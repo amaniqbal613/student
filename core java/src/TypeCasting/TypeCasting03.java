de06358f5b4e8c8e7b5956138726c2c9fcac2887
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-08 23:17:29 +0530

    TypeCasting with question

---
+++ core java/src/TypeCasting/TypeCasting03.java
@@ -0,0 +1,12 @@
+//	3.	wap to declare tow char variable and assign a value then add these two variables and print the sum
+package TypeCasting;
+
+public class TypeCasting03 {
+    public static void main(String[] args) {
+        char c='e';
+        char a='f';
+        int sum=c+a;
+        System.out.println("result="+sum);
+
+    }
+}
