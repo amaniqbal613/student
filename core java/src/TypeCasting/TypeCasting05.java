00e5dc6754dffeb84fce1ec3eb18f114ff1738bf
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-08 23:26:24 +0530

    TypeCasting with question

---
+++ core java/src/TypeCasting/TypeCasting05.java
@@ -0,0 +1,10 @@
+//	5.wap to define a short variable and assign its value to an int type variable and print both variables.
+package TypeCasting;
+
+public class TypeCasting05 {
+    public static void main(String[] args) {
+        short s='d';
+        int i=(int)s;
+        System.out.println("result="+i);
+    }
+}
