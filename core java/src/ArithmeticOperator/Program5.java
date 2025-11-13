d5dfd5d9d4453dbc747b621cbb08246100d4ce7f
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-13 21:32:58 +0530

    Arithmetic Operator with question

---
+++ core java/src/ArithmeticOperator/Program5.java
@@ -0,0 +1,12 @@
+//	5.	wap to multiply two numbers?
+package ArithmeticOperator;
+
+public class Program5 {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=5;
+        b=2;
+        c=a*b;
+        System.out.println(c);
+    }
+}
