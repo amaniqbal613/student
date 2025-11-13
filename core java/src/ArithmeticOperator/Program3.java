ef2dd54bb23f173b6e8f2996178916163c37bbb1
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-13 21:28:39 +0530

    Arithmetic Operator with question

---
+++ core java/src/ArithmeticOperator/Program3.java
@@ -0,0 +1,12 @@
+//	3.	wap to subtract two numbers?
+package ArithmeticOperator;
+
+public class Program3 {
+    public static void main(String[] args) {
+     int a,b,c;
+     a=85;
+     b=67;
+     c=a-b;
+        System.out.println(c);
+    }
+}
