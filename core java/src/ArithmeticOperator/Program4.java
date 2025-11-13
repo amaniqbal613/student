688c0fa5c5509c70c58f651fab0b37f5c24a38c7
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-13 21:30:59 +0530

    Arithmetic Operator with question

---
+++ core java/src/ArithmeticOperator/Program4.java
@@ -0,0 +1,13 @@
+//	4.	wap to subtract three numbers?
+package ArithmeticOperator;
+
+public class Program4 {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a=965;
+        b=123;
+        c=432;
+        d=a-b-c;
+        System.out.println(d);
+    }
+}
