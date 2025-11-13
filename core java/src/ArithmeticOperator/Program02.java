ba671089f6d5b1f2ecfa8de27bccd2ff9be4da20
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-13 21:21:41 +0530

    Arithmetic Operator with question

---
+++ core java/src/ArithmeticOperator/Program02.java
@@ -0,0 +1,13 @@
+//	2.	wap to add three numbers?
+package ArithmeticOperator;
+
+public class Program02 {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a=56;
+        b=87;
+        c=56;
+        d=a+b+c;
+        System.out.println(d);
+    }
+}
