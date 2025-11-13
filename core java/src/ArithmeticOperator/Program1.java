a32be0edc03993315786f4983b5ef1e21260b5a1
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-13 21:18:39 +0530

    Arithmetic Operator with question

---
+++ core java/src/ArithmeticOperator/Program1.java
@@ -0,0 +1,11 @@
+//	1.	wap to add two numbers?
+package ArithmeticOperator;
+
+public class Program1 {
+    public static void main(String[] args) {
+        int a=23;
+        int b=45;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
