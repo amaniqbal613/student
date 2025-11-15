2e1dd256787cb9d127de6cbf10ec061ae26fe2d7
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-15 12:43:26 +0530

    pre post increment with question

---
+++ core java/src/PrePostincrement/program01.java
@@ -0,0 +1,16 @@
+//	1.	wap to do pre increment a number and assign its value to other number and print both number
+package PrePostincrement;
+
+public class program01 {
+    public static void main(String[] args) {
+        System.out.println("pre increment");
+        int a=34;
+        int b=++a;
+        System.out.println(b);
+        //	2.	wap to do post increment a number and assign its value to other number and print both number
+        System.out.println("post increment");
+        int c=45;
+        int d=c++;
+        System.out.println(d);
+    }
+}
