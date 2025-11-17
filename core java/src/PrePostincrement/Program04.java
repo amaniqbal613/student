11accf8f52789901eb67448ed48399b74cbd5402
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-17 22:53:43 +0530

    pre post increment with question

---
+++ core java/src/PrePostincrement/Program04.java
@@ -0,0 +1,12 @@
+//8.	int a = 3;
+//		int b = ++a;
+//		what will be the value of a and b on printing
+package PrePostincrement;
+
+public class Program04 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = ++a;
+        System.out.println(b);
+    }
+}
