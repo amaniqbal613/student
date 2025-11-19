a60e0250be0b94ba108680f7946fffdec07b62c2
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-19 11:59:16 +0530

    relational operators with question

---
+++ core java/src/RelationalOperators2/Program2.java
@@ -0,0 +1,20 @@
+//int a = 10;
+//		int b = 15;
+//		a++;
+//		b--;
+//		int c = b % a;
+//		System.out.println(c >= b);
+//
+package RelationalOperators2;
+
+public class Program2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        a++;
+        b--;
+        int c = b % a;
+        System.out.println(c >= b);
+
+    }
+}
