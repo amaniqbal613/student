613b665b1f2b0b62d4d1fc96c52aaaf5d23a4bb3
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-19 11:57:49 +0530

    relational operators with question

---
+++ core java/src/RelationalOperators2/Program01.java
@@ -0,0 +1,14 @@
+//int a = 10;
+//		int b = a - 5;
+//		System.out.println(a <= b);
+//		System.out.println(a > b);
+package RelationalOperators2;
+
+public class Program01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a - 5;
+        System.out.println(a <= b);
+        System.out.println(a > b);
+    }
+}
