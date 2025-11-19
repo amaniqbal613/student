f08b4727d825e6704e7d68a3d02f09dc80207bba
amaniqbal613 <amaniqbal466@gmail.com>
2025-11-19 12:18:28 +0530

    relational operators with question

---
+++ core java/src/RelationalOperators2/Program3.java
@@ -0,0 +1,18 @@
+//5.	int a = 10;
+//boolean x = a++==10;
+//boolean y = --a==10;
+//sop(a);
+//sop(x);
+//sop(y);
+package RelationalOperators2;
+
+public class Program3 {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean x = a++==10;
+        boolean y = --a==10;
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
