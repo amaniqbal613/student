cb6b195c7bfbdf3834e9620a79a3c9f6586c5630
amaniqbal613 <amaniqbal466@gmail.com>
2025-12-14 23:31:58 +0530

    Logical operators with question

---
+++ core java/src/LogicalOperator/Program05.java
@@ -0,0 +1,22 @@
+//5.	boolean a = true;
+//		boolean b = true;
+//		boolean c = true;
+//		boolean d = false;
+//		boolean e = a || !b || c || !d;
+//		print a, b, c, d and e
+package LogicalOperator;
+
+public class Program05 {
+    public static void main(String[] args) {
+        boolean a=true;
+        boolean b=true;
+        boolean c=true;
+        boolean d=false;
+        boolean e=a ||!b || c || !d;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
