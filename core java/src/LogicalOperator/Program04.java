31bae1bbf4c7c4611836115ef06a5dc52a615efd
amaniqbal613 <amaniqbal466@gmail.com>
2025-12-14 23:31:44 +0530

    Logical operators with question

---
+++ core java/src/LogicalOperator/Program04.java
@@ -0,0 +1,17 @@
+//4.	int a = 1;
+//		int b = 2;
+//		if(a!=1 && b++==3){
+//			sop("hello world");
+//		}
+//		what will be the value of a and b on printing
+package LogicalOperator;
+
+public class Program04 {
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        if (a!=1 && b++==3){
+            System.out.println("hello world");
+        }
+    }
+}
