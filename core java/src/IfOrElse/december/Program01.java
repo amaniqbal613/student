10c71a53b6c32459504adcd8d9aaff21ed9ab475
amaniqbal613 <amaniqbal466@gmail.com>
2025-12-21 12:39:16 +0530

    if or else with question

---
+++ core java/src/IfOrElse/december/Program01.java
@@ -0,0 +1,20 @@
+//1.	wap to find which number is greater among two numbers
+package IfOrElse.december;
+
+import java.util.Scanner;
+
+public class Program01 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number ");
+        int num1= sc.nextInt();
+        System.out.println("enter the second number");
+        int num2= sc.nextInt();
+        if (num1>num2){
+            System.out.println(num1+" is greater");
+        }
+        else
+            System.out.println(num2+" is greater");
+    }
+
+}
