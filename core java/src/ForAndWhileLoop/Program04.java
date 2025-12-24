00053a6017495cce50d349c6f93cdaf25faf5f6e
amaniqbal613 <amaniqbal466@gmail.com>
2025-12-24 11:59:01 +0530

    for loop with question

---
+++ core java/src/ForAndWhileLoop/Program04.java
@@ -0,0 +1,11 @@
+//	4.	wap to print first five even numbers.
+package ForAndWhileLoop;
+
+public class Program04 {
+    public static void main(String[] args) {
+        System.out.println("first five even numbers");
+        for (int i=2;i<=10;i+=2){
+            System.out.println("i="+i);
+        }
+    }
+}
