dd2e194a2a4dcd40c1842e6c6a43d3be35a5ece0
amaniqbal613 <amaniqbal466@gmail.com>
2025-12-24 11:59:18 +0530

    for loop with question

---
+++ core java/src/ForAndWhileLoop/Program05.java
@@ -0,0 +1,11 @@
+//	5.	wap to print to print all negative numbers between -1 and -10.
+package ForAndWhileLoop;
+
+public class Program05 {
+    public static void main(String[] args) {
+        System.out.println("negative numbers between -1 to -10");
+        for (int i=-1;i>=-10;i--){
+            System.out.println("i="+i);
+        }
+    }
+}
