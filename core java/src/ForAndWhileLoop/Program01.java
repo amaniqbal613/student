77f92da50ac26b05cedbcbd3a00bd7c176387bb5
amaniqbal613 <amaniqbal466@gmail.com>
2025-12-24 11:57:51 +0530

    for loop with question

---
+++ core java/src/ForAndWhileLoop/Program01.java
@@ -0,0 +1,11 @@
+//1.	wap to print number from 1 to 10
+package ForAndWhileLoop;
+
+public class Program01 {
+    public static void main(String[] args) {
+        System.out.println("1 to 10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("i=" + i);
+        }
+    }
+}
