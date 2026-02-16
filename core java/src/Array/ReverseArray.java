1ac0eb79496e09d4fe27a73bf3d8cd951bf0c46f
amaniqbal613 <amaniqbal466@gmail.com>
2026-02-16 23:31:17 +0530

    Array program with question

---
+++ core java/src/Array/ReverseArray.java
@@ -0,0 +1,11 @@
+//	16) 	wap to reverse an array
+package Array;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int arr[]={3,2,4,5,1};
+        for (int i=4;i< arr.length;i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
